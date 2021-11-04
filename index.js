441e4a15efb1f5dc20de83f721f1210b40e8c848
Weeping23 <javierseijas23@gmail.com>
2021-11-04 15:47:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function btn1() {
+    var audio = document.getElementById("aud1");
+   audio.play();
+   }
+   function btn2() {
+    var audio = document.getElementById("aud2");
+   audio.play();
+   }
+   function btn3() {
+    var audio = document.getElementById("aud3");
+   audio.play();
+   }
+   function btn4() {
+    var audio = document.getElementById("aud4");
+   audio.play();
+   }
+   function btn5() {
+    var audio = document.getElementById("aud5");
+   audio.play();
+   }
+   function btn6() {
+    var audio = document.getElementById("aud6");
+   audio.play();
+   }
+   function btn7() {
+    var audio = document.getElementById("aud7");
+   audio.play();
+   }
+   function btn8() {
+    var audio = document.getElementById("aud8");
+   audio.play();
+   }
+   function btn9() {
+    var audio = document.getElementById("aud9");
+   audio.play();
+   }
+   function btn10() {
+    var audio = document.getElementById("aud10");
+   audio.play();
+   }
